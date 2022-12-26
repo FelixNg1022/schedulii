8574aefab3cbf0552aeff3405050dc2363f55ab7
Eric Lu <112150559+erickexinlu@users.noreply.github.com>
2022-12-25 16:41:30 -0800

    feat: front end landing page (#5)
    * SCH-13 feat: basic skeleton with flexbox
    
    * SCH-13 feat: complete very basic landing page
    
    * SCH-13 fix: removed non-essential test
    
    * SCH-13 fix: removed extra imports
    
    * SCH-13 feat: additional styling

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css'
+
+const LandingPage = () => {
+  return (
+    <div className='wrapper'>
+        <div className='header'></div>
+        <div className='title'>
+            <span>Schedulii</span>            
+            <div className='loginButton'>Login with Google</div>
+        </div>
+        
+        <div className='footer'></div>
+    </div>
+  )
+}
+
+export default LandingPage
