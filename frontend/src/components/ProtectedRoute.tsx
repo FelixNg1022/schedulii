4a0514e9d3a76763d32d9283c2782e9178bdadde
Louie Weng <louieweng@outlook.com>
2022-12-30 20:50:21 -0800

    fix: refactored calendar and added more lint rules (#18)
    * refactored calendar and added more lint rules
    
    * fixed bugs with type imports

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -2,9 +2,9 @@ import type { ReactElement } from "react";
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import type Props from "../types/Props";
+import type AuthProviderPropsType from "../context/AuthProviderPropsType";
 
-const ProtectedRoute = ({ children }: Props): ReactElement => {
+const ProtectedRoute = ({ children }: AuthProviderPropsType): ReactElement => {
     const token = useAuth();
     const location = useLocation();
 
--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,10 +1,10 @@
 import type { ReactElement } from "react";
 import React from "react";
 import { AuthContext } from "./AuthContext";
-import type Props from "../types/Props";
-import type { AppContextInterface } from "./AuthContext";
+import type AppContextDataType from "./AuthContextDataType";
+import type AuthProviderPropsType from "./AuthProviderPropsType";
 
-const AuthProvider = ({ children }: Props): ReactElement => {
+const AuthProvider = ({ children }: AuthProviderPropsType): ReactElement => {
     const [token, setToken] = React.useState(null);
 
     const handleLogin = async (): Promise<void> => {
@@ -23,7 +23,7 @@ const AuthProvider = ({ children }: Props): ReactElement => {
         setToken(null);
     };
 
-    const value: AppContextInterface = {
+    const value: AppContextDataType = {
         token,
         onLogin: handleLogin,
         onLogout: handleLogout
